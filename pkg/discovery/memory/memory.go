79ca36f0861aa949361c9d6de27e4343e998ecaf
Max <37843972+mxstrv@users.noreply.github.com>
2024-10-28 16:26:58 +0300

    chore: fill empty methods

--- pkg/discovery/memory/memory.go
+++ pkg/discovery/memory/memory.go
@@ -2,6 +2,8 @@ package memory
 
 import (
 	"context"
+	"errors"
+	"movieapp/pkg/discovery"
 	"sync"
 	"time"
 )
@@ -33,16 +35,42 @@ func (r *Registry) Register(ctx context.Context, instanceID string, serviceName 
 
 // Deregister removes a service record in the registry
 func (r *Registry) Deregister(ctx context.Context, instanceID string, serviceName string) error {
+	r.Lock()
+	defer r.Unlock()
+	if _, ok := r.serviceAddrs[serviceName]; !ok {
+		return nil
+	}
+	delete(r.serviceAddrs[serviceName], instanceID)
 	return nil
-	// TODO: implement
 }
 
+// ServiceAddresses returns list of addresses of active instances of a given service.
 func (r *Registry) ServiceAddresses(ctx context.Context, serviceID string) ([]string, error) {
-	return nil, nil
-	// TODO: implement
+	r.RLock()
+	defer r.RUnlock()
+	if len(r.serviceAddrs[serviceID]) == 0 {
+		return nil, discovery.ErrNotFound
+	}
+	var res []string
+	for _, ins := range r.serviceAddrs[serviceID] {
+		// Hardcoded 5 seconds
+		if ins.lastActive.Before(time.Now().Add(5 * time.Second)) {
+			continue
+		}
+		res = append(res, ins.hostPort)
+	}
+	return res, nil
 }
 
 func (r *Registry) ReportHealthyState(instanceID string, serviceName string) error {
+	r.Lock()
+	defer r.Unlock()
+	if _, ok := r.serviceAddrs[serviceName]; !ok {
+		return errors.New("service is not registered")
+	}
+	if _, ok := r.serviceAddrs[serviceName][instanceID]; !ok {
+		return errors.New("instance is not registered")
+	}
+	r.serviceAddrs[serviceName][instanceID].lastActive = time.Now()
 	return nil
-	//TODO: implement
 }
