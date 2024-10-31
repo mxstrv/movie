3598e1db67cbb072aba0b2094950fb272ea6e4ce
Max <37843972+mxstrv@users.noreply.github.com>
2024-11-01 00:10:42 +0300

    feat: implement discovery service in main.go

--- metadata/cmd/main.go
+++ metadata/cmd/main.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"movieapp/metadata/internal/controller/metadata"
 	httphandler "movieapp/metadata/internal/handler/http"
 	"movieapp/metadata/internal/repository/memory"
+	"movieapp/pkg/discovery"
+	"movieapp/pkg/discovery/consul"
 	"net/http"
+	"time"
 )
 
+const serviceName = "metadata"
+
 func main() {
-	log.Println("Starting the movie metadata service")
+	var port int
+	flag.IntVar(&port, "port", 8081, "API handler port")
+	flag.Parse()
+	log.Printf("Starting the movie metadata service on port %d", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(
+		ctx,
+		instanceID,
+		serviceName,
+		fmt.Sprintf("http://localhost:%d", port),
+	); err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer func(registry *consul.Registry, ctx context.Context, instanceID string, _ string) {
+		_ = registry.Deregister(ctx, instanceID, serviceName)
+	}(registry, ctx, instanceID, serviceName)
+
 	repo := memory.New()
 	ctrl := metadata.New(repo)
 	h := httphandler.New(ctrl)
--- rating/cmd/main.go
+++ rating/cmd/main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
+	"movieapp/pkg/discovery"
+	"movieapp/pkg/discovery/consul"
 	"movieapp/rating/internal/controller/rating"
 	httphandler "movieapp/rating/internal/handler/http"
 	"movieapp/rating/internal/repository/memory"
 	"net/http"
+	"time"
 )
 
+const serviceName = "rating"
+
 func main() {
-	log.Println("Starting Rating service")
+	var port int
+	flag.IntVar(&port, "port", 8082, "API handler port")
+	flag.Parse()
+	log.Printf("Starting Rating service on port %d", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(
+		ctx,
+		instanceID,
+		serviceName,
+		fmt.Sprintf("http://localhost:%d", port),
+	); err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer func(registry *consul.Registry, ctx context.Context, instanceID string, _ string) {
+		_ = registry.Deregister(ctx, instanceID, serviceName)
+	}(registry, ctx, instanceID, serviceName)
 	repo := memory.New()
 	ctrl := rating.New(repo)
 	h := httphandler.New(ctrl)
