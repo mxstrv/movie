6ecfeb5081573e0907781e6aab7ad83d700a5bfa
Max <37843972+mxstrv@users.noreply.github.com>
2024-11-11 00:14:42 +0300

    chore: proper implementation of main func with Consul enabled

--- metadata/cmd/main.go
+++ metadata/cmd/main.go
@@ -1,67 +1,66 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
 	"movieapp/gen"
 	"movieapp/metadata/internal/controller/metadata"
 	grpchandler "movieapp/metadata/internal/handler/grpc"
 	"movieapp/metadata/internal/repository/memory"
+	"movieapp/pkg/discovery"
+	"movieapp/pkg/discovery/consul"
 	"net"
+	"time"
 )
 
-//const serviceName = "metadata"
-//
-//func main() {
-//	var port int
-//	flag.IntVar(&port, "port", 8081, "API handler port")
-//	flag.Parse()
-//	log.Printf("Starting the movie metadata service on port %d", port)
-//	registry, err := consul.NewRegistry("localhost:8500")
-//	if err != nil {
-//		panic(err)
-//	}
-//	ctx := context.Background()
-//	instanceID := discovery.GenerateInstanceID(serviceName)
-//	if err := registry.Register(
-//		ctx,
-//		instanceID,
-//		serviceName,
-//		fmt.Sprintf("localhost:%d", port),
-//	); err != nil {
-//		panic(err)
-//	}
-//	go func() {
-//		for {
-//			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
-//				log.Println("Failed to report healthy state: " + err.Error())
-//			}
-//			time.Sleep(1 * time.Second)
-//		}
-//	}()
-//	defer func(registry *consul.Registry, ctx context.Context, instanceID string, _ string) {
-//		_ = registry.Deregister(ctx, instanceID, serviceName)
-//	}(registry, ctx, instanceID, serviceName)
-//
-//	repo := memory.New()
-//	ctrl := metadata.New(repo)
-//	h := httphandler.New(ctrl)
-//	http.Handle("/metadata", http.HandlerFunc(h.GetMetadata))
-//	if err := http.ListenAndServe(fmt.Sprintf("localhost:%d", port), nil); err != nil {
-//		panic(err)
-//	}
-//}
+const serviceName = "metadata"
 
 func main() {
-	log.Println("Starting movie metadata service")
+	var port int
+	flag.IntVar(&port, "port", 8081, "API handler port")
+	flag.Parse()
+	log.Printf("Starting the movie metadata service on port %d", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(
+		ctx,
+		instanceID,
+		serviceName,
+		fmt.Sprintf("localhost:%d", port),
+	); err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer func(registry *consul.Registry, ctx context.Context, instanceID string, _ string) {
+		_ = registry.Deregister(ctx, instanceID, serviceName)
+	}(registry, ctx, instanceID, serviceName)
+
 	repo := memory.New()
 	ctrl := metadata.New(repo)
 	h := grpchandler.New(ctrl)
-	lis, err := net.Listen("tcp", "localhost:8081")
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%v", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	srv := grpc.NewServer()
+	reflection.Register(srv)
 	gen.RegisterMetadataServiceServer(srv, h)
-	srv.Serve(lis)
+	if err := srv.Serve(lis); err != nil {
+		panic(err)
+	}
 }
--- rating/cmd/main.go
+++ rating/cmd/main.go
@@ -1,66 +1,59 @@
 package main
 
 import (
-	"google.golang.org/grpc"
+	"context"
+	"flag"
+	"fmt"
 	"log"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"movieapp/gen"
+	"movieapp/pkg/discovery"
+	"movieapp/pkg/discovery/consul"
 	"movieapp/rating/internal/controller/rating"
 	grpchandler "movieapp/rating/internal/handler/grpc"
 	"movieapp/rating/internal/repository/memory"
-	"net"
 )
 
-//const serviceName = "rating"
-//
-//func main() {
-//	var port int
-//	flag.IntVar(&port, "port", 8082, "API handler port")
-//	flag.Parse()
-//	log.Printf("Starting Rating service on port %d", port)
-//	registry, err := consul.NewRegistry("localhost:8500")
-//	if err != nil {
-//		panic(err)
-//	}
-//	ctx := context.Background()
-//	instanceID := discovery.GenerateInstanceID(serviceName)
-//	if err := registry.Register(
-//		ctx,
-//		instanceID,
-//		serviceName,
-//		fmt.Sprintf("localhost:%d", port),
-//	); err != nil {
-//		panic(err)
-//	}
-//	go func() {
-//		for {
-//			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
-//				log.Println("Failed to report healthy state: " + err.Error())
-//			}
-//			time.Sleep(1 * time.Second)
-//		}
-//	}()
-//	defer func(registry *consul.Registry, ctx context.Context, instanceID string, _ string) {
-//		_ = registry.Deregister(ctx, instanceID, serviceName)
-//	}(registry, ctx, instanceID, serviceName)
-//	repo := memory.New()
-//	ctrl := rating.New(repo)
-//	h := httphandler.New(ctrl)
-//	http.Handle("/ratings", http.HandlerFunc(h.Handle))
-//	if err := http.ListenAndServe(fmt.Sprintf("localhost:%d", port), nil); err != nil {
-//		panic(err)
-//	}
-//}
+const serviceName = "rating"
 
 func main() {
-	log.Println("Starting movie rating service")
+	var port int
+	flag.IntVar(&port, "port", 8082, "API handler port")
+	flag.Parse()
+	log.Printf("Starting the rating service on port %d", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer registry.Deregister(ctx, instanceID, serviceName)
 	repo := memory.New()
 	ctrl := rating.New(repo)
 	h := grpchandler.New(ctrl)
-	lis, err := net.Listen("tcp", "localhost:8082")
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%v", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	srv := grpc.NewServer()
+	reflection.Register(srv)
 	gen.RegisterRatingServiceServer(srv, h)
-	srv.Serve(lis)
+	if err := srv.Serve(lis); err != nil {
+		panic(err)
+	}
 }
