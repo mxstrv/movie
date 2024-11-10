191ddd8f919751fba817d7a9d861850acd03b59a
Max <37843972+mxstrv@users.noreply.github.com>
2024-11-11 01:41:23 +0300

    feat: implement gRPC handler for movie service

---
+++ movie/internal/handler/grpc/grpc.go
@@ -0,0 +1,41 @@
+package grpc
+
+import (
+	"context"
+	"errors"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"movieapp/gen"
+	"movieapp/metadata/pkg/model"
+	"movieapp/movie/internal/controller/movie"
+)
+
+// Handler defines a movie gRPC handler.
+type Handler struct {
+	gen.UnimplementedMovieServiceServer
+	ctrl *movie.Controller
+}
+
+func New(ctrl *movie.Controller) *Handler {
+	return &Handler{ctrl: ctrl}
+}
+
+// GetMovieDetails returns movie details by id.
+func (h *Handler) GetMovieDetails(ctx context.Context, req *gen.GetMovieDetailsRequest) (*gen.GetMovieDetailsResponse, error) {
+	if req == nil || req.MovieId == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "nil req or empty id")
+	}
+	m, err := h.ctrl.Get(ctx, req.MovieId)
+	if err != nil && errors.Is(err, movie.ErrNotFound) {
+		return nil, status.Errorf(codes.NotFound, err.Error())
+	} else if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	return &gen.GetMovieDetailsResponse{
+		MovieDetails: &gen.MovieDetails{
+			Metadata: model.MetadataToProto(&m.Metadata),
+			Rating:   float32(*m.Rating),
+		},
+	}, nil
+}
