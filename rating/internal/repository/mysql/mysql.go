a3d7dfe38a0b0175e9bbcfe8696d7f9368124e78
Max <37843972+mxstrv@users.noreply.github.com>
2024-11-28 21:58:48 +0300

    feat: add mysql support for rating service

---
+++ rating/internal/repository/mysql/mysql.go
@@ -0,0 +1,60 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"movieapp/rating/internal/repository"
+	"movieapp/rating/pkg/model"
+	"os"
+)
+
+// Repository defines a MySQL-bases rating repository.
+type Repository struct {
+	db *sql.DB
+}
+
+// New creates a new MySQL-based repository.
+func New(db *sql.DB) (*Repository, error) {
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD") // TODO: implement better solution.
+	dbName := os.Getenv("DB_NAME")
+	db, err := sql.Open("mysql", dbUsername+":"+dbPassword+"@/"+dbName)
+	if err != nil {
+		return nil, err
+	}
+	return &Repository{db: db}, nil
+}
+
+// Get retrieves all ratings for a given record.
+func (r *Repository) Get(ctx context.Context, recordID model.RecordID, recordType model.RecordType) ([]model.Rating, error) {
+	rows, err := r.db.QueryContext(ctx, "SELECT user_id, value FROM ratings WHERE record_id = ? AND record_type = ?", recordID, recordType)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var res []model.Rating
+	for rows.Next() {
+		var userID string
+		var value int32
+		if err := rows.Scan(&userID, &value); err != nil {
+			return nil, err
+		}
+		res = append(res, model.Rating{
+			UserID: model.UserID(userID),
+			Value:  model.RatingValue(value),
+		})
+	}
+	if len(res) == 0 {
+		return nil, repository.ErrNotFound
+	}
+	return res, nil
+}
+
+// Put add a movie rating for a given record.
+func (r *Repository) Put(ctx context.Context, recordID model.RecordID, recordType model.RecordType, ratings *model.Rating) error {
+	_, err := r.db.ExecContext(ctx, "INSERT INTO ratings (record_id, record_type, user_id, value) VALUES (?, ?, ?, ?)",
+		recordID, recordType, ratings.UserID, ratings.Value)
+	return err
+
+}
